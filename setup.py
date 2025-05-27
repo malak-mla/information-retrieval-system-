829e678a58c6aa523f2e375551f6c54688782544
Malak Mla <malek.mla@outlook.fr>
2025-05-27 23:01:28 +0100

    requirements added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Generative AI Project',
+    version= '0.0.0',
+    author= 'Malak',
+    author_email= 'malek.mla@outlook.fr',
+    packages= find_packages(),
+    install_requires = []
+
+)
